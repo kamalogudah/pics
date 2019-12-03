4a0869c9c063dcf736e0d2baac6110264845a4c4
kamalogudah <mcpaul2058@gmail.com>
2019-12-03 10:02:40 +0300

    Axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
 
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 58efbb6ba848923e35431f889b82914702afde6dc4bab980a7a3335aa43918c3'
+      }
+    });
 
   }
 
